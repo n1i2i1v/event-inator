8012680e0a43372dc43153c6f4887c3e552ee175
Lilit Otaryan <lilit_otaryan@edu.aua.am>
2019-07-18 15:31:21 +0400

    Ticket schema updated.

--- schemas/tickets.js
+++ schemas/tickets.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const TicketSchema = new mongoose.Schema({
+  cost: {
+    type: Number,
+    required: true
+  },
 });
 
 const Ticket = mongoose.model('Ticket', TicketSchema);
